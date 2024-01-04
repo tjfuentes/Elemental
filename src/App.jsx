35fd717d5cc61533cba99ab4e2a07ce0769745a3
Tynneth Jhoel <tj.fuentes15@gmail.com>
2024-01-04 22:48:28 +0800

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,99 @@
+import "./css/app.css";
+
+function App() {
+  return (
+    <>
+      <main className="game-container">
+        <h1 className="difficulty">Difficulty: Easy</h1>
+        <div className="play-now">
+          <img src="/fire-element.jfif" alt="img-1" />
+          <img src="/water.jfif" alt="img-2" />
+          <img src="/fire-element.jfif" alt="img-3" />
+          <img src="/Wind.jfif" alt="img-4" />
+          <img src="/fire-element.jfif" alt="img-5" />
+          <img src="/earth.jfif" alt="img-6" />
+          <img src="/fire-element.jfif" alt="img-7" />
+          <img src="/fire-element.jfif" alt="img-8" />
+          <img src="/Wind.jfif" alt="img-9" />
+          <img src="/earth.jfif" alt="img-10" />
+          <img src="/water.jfif" alt="img-11" />
+          <img src="/Wind.jfif" alt="img-12" />
+          <img src="/earth.jfif" alt="img-13" />
+          <img src="/water.jfif" alt="img-14" />
+          <img src="/earth.jfif" alt="img-15" />
+          <img src="/earth.jfif" alt="img-16" />
+          <img src="/water.jfif" alt="img-17" />
+          <img src="/fire-element.jfif" alt="img-18" />
+          <img src="/earth.jfif" alt="img-19" />
+          <img src="/Wind.jfif" alt="img-20" />
+          <img src="/Wind.jfif" alt="img-21" />
+          <img src="/earth.jfif" alt="img-22" />
+          <img src="/fire-element.jfif" alt="img-23" />
+          <img src="/water.jfif" alt="img-24" />
+          <img src="/water.jfif" alt="img-25" />
+          <img src="/fire-element.jfif" alt="img-26" />
+          <img src="/water.jfif" alt="img-27" />
+          <img src="/Wind.jfif" alt="img-28" />
+          <img src="/fire-element.jfif" alt="img-29" />
+          <img src="/water.jfif" alt="img-30" />
+          <img src="/water.jfif" alt="img-31" />
+          <img src="/earth.jfif" alt="img-32" />
+          <img src="/Wind.jfif" alt="img-33" />
+          <img src="/fire-element.jfif" alt="img-34" />
+          <img src="/water.jfif" alt="img-35" />
+          <img src="/Wind.jfif" alt="img-36" />
+          <img src="/fire-element.jfif" alt="img-37" />
+          <img src="/Wind.jfif" alt="img-38" />
+          <img src="/water.jfif" alt="img-39" />
+          <img src="/earth.jfif" alt="img-40" />
+          <img src="/Wind.jfif" alt="img-41" />
+          <img src="/fire-element.jfif" alt="img-42" />
+          <img src="/water.jfif" alt="img-43" />
+          <img src="/earth.jfif" alt="img-44" />
+          <img src="/Wind.jfif" alt="img-45" />
+          <img src="/earth.jfif" alt="img-46" />
+          <img src="/fire-element.jfif" alt="img-47" />
+          <img src="/water.jfif" alt="img-48" />
+          <img src="/Wind.jfif" alt="img-49" />
+          <img src="/earth.jfif" alt="img-50" />
+          <img src="/fire-element.jfif" alt="img-51" />
+          <img src="/earth.jfif" alt="img-52" />
+          <img src="/Wind.jfif" alt="img-53" />
+          <img src="/fire-element.jfif" alt="img-54" />
+          <img src="/earth.jfif" alt="img-55" />
+          <img src="/fire-element.jfif" alt="img-56" />
+          <img src="/water.jfif" alt="img-57" />
+          <img src="/water.jfif" alt="img-58" />
+          <img src="/earth.jfif" alt="img-59" />
+          <img src="/Wind.jfif" alt="img-60" />
+          <img src="/Wind.jfif" alt="img-61" />
+          <img src="/Wind.jfif" alt="img-62" />
+          <img src="/water.jfif" alt="img-63" />
+          <img src="/earth.jfif" alt="img-64" />
+          <img src="/fire-element.jfif" alt="img-65" />
+          <img src="/Wind.jfif" alt="img-66" />
+          <img src="/water.jfif" alt="img-67" />
+          <img src="/fire-element.jfif" alt="img-68" />
+          <img src="/water.jfif" alt="img-69" />
+          <img src="/earth.jfif" alt="img-70" />
+          <img src="/fire-element.jfif" alt="img-71" />
+          <img src="/water.jfif" alt="img-72" />
+          <img src="/earth.jfif" alt="img-73" />
+          <img src="/Wind.jfif" alt="img-74" />
+          <img src="/Wind.jfif" alt="img-75" />
+          <img src="/earth.jfif" alt="img-76" />
+          <img src="/fire-element.jfif" alt="img-77" />
+          <img src="/earth.jfif" alt="img-78" />
+          <img src="/Wind.jfif" alt="img-79" />
+          <img src="/water.jfif" alt="img-80" />
+        </div>
+        <div className="add-info">
+          <p className="pair">Pair Remaining: 40</p>
+          <p className="time-">Time: 50</p>
+        </div>
+      </main>
+    </>
+  );
+}
+
+export default App;
